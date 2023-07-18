5dadfa59c5fcc1ef965faab3e8f2c8be0d2fc533
sko <59856799+skoisgood@users.noreply.github.com>
2023-07-18 16:29:15 +0700

    test

--- src/pages/MPindividual.tsx
+++ src/pages/MPindividual.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import PageHeader from "../components/PageHeader";
 
 const MPindividual = () => {
   return (
     <>
+      <PageHeader name="ข้อมูลคนหาย" />
       <div>MPindividual</div>
     </>
   );
